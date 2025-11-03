eb48880c1240ff2aad14fac48667c3e30aadddad
Sl4cK0TH <davidbelle772@gmail.com>
2025-11-03 11:20:04 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/odiorent/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.odiorent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  // This line is required to ensure Flutter is initialized
+  // before you call Supabase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: 'https://oxxjpcjusuemhdjpyssy.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eGpwY2p1c3VlbWhkanB5c3N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NTE2NTcsImV4cCI6MjA3NzEyNzY1N30.46FVUi8lYv3UGR4vC6gc3W1tAMm3Jv7DGmPVV21jPGQ',
+  );
+
+  runApp(const MyApp());
+}
+
+// You can create a global variable to access the Supabase client
+// from anywhere in your app
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OdioRent',
+      home: Scaffold(
+        body: Center(
+          child: Text("OdioRent App Initialized!"),
+        ),
+      ),
+    );
+  }
+}
